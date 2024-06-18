2f0538f9f90472cd05a6911dca71eae6f2b59abb
SvZor <zorinasp@yandex.ru>
2024-06-18 13:45:35 +0900

    удаление pop

--- main2.py
+++ main2.py
@@ -6,6 +6,15 @@ print(list_1)
 # # добавлять значения в список - append -  в конец списка
 # list_1.append(10)
 # print(list_1)
-for i in range(5):
-    list_1.append(i)
-    print(list_1)
+# for i in range(5):            #цикл добавляет цифры от 0 до 4
+#     list_1.append(i)
+#     print(list_1)   #печатает каждое добавление
+
+# ## удаление последнего элемента списка 
+# list_1 = [12, 7, -1, 21, 0]
+# print(list_1.pop()) #0
+# print(list_1) # [12, 7 -1, 21]
+# print(list_1.pop()) #21
+# print(list_1) # [12, 7, -1]
+# print(list_1.pop()) #-1
+# print(list_1) # [12, 7]
