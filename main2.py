39960dbfe2c678822ec0e60fbe3bd04b04e3246a
SvZor <zorinasp@yandex.ru>
2024-06-18 14:15:16 +0900

    работа со срезами

--- main2.py
+++ main2.py
@@ -29,3 +29,19 @@ print(list_1)
 # print(list_1.insert(2, 17))
 # print(list_1) # [12, 7, 17, -1, 21, 0]
 
+## работа со срезами
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list_1[0]) # 1
+print(list_1[1]) # 2
+print(list_1[len(list_1)-1]) # 10
+print(list_1[-5]) # 6
+print(list_1[:]) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list_1[:2]) # [1, 2]
+print(list_1[len(list_1)-2:]) #[9, 10]
+print(list_1[2:9]) # [3, 4, 5, 6, 7, 8, 9]
+print(list_1[6:-18]) # []
+print(list_1[0:len(list_1):6]) # [1, 7]
+print(list_1[::6]) # [1, 7]
+
+
+
