2cd722f2a5bf175c6f738596925b0df2c01af338
SvZor <zorinasp@yandex.ru>
2024-06-18 16:42:51 +0900

    задача19

--- main3.py
+++ main3.py
@@ -4,6 +4,37 @@
 # Input: [1, 1, 2, 0, -1, 3, 4, 4]
 # Output: 6
 
-list1 = [1, 1, 2, 0, -1, 3, 4, 4]
-print(len(set(list1)))
+# list1 = [1, 1, 2, 0, -1, 3, 4, 4]
+# print(len(set(list1)))
 
+# Задача №19. Решение в группах
+# Дана последовательность из N целых чисел и число
+# K. Необходимо сдвинуть всю последовательность
+# (сдвиг - циклический) на K элементов вправо, K –
+# положительное число.
+# Input: [1, 2, 3, 4, 5] k = 3
+# Output: [4, 5, 1, 2, 3]
+
+# # решение однокурсников
+# user_list = [1, 2, 3, 4, 5]
+# k = 3
+# res_list=[]
+# res_list.extend(user_list[k:len(user_list)])
+# res_list.extend(user_list[0:k])
+# print(res_list)
+
+# ## решение учителя неверное, т к 5 и 4 не переворачиваются
+# list1 = [5, 4, 6, 7, -10]
+# k = int(input())
+# k = k % len(list1)
+
+# list_res = []
+# for i in range(k):
+#     list_res.append(list1[len(list1) - 1 - i])
+
+# print(list_res)
+
+# for i in range(len(list1) - k):
+#     list_res.append(list1[i])
+
+# print(list_res)
